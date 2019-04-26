9446dfceec45ed88d943d15f483cda16732528f9
Rigsto <rigsto@users.noreply.github.com>
2019-04-26 17:03:42 +0700

    login bug fixes

--- app/src/main/java/com/aurigaaristo/taxiportfinal/LoginActivity.java
+++ app/src/main/java/com/aurigaaristo/taxiportfinal/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.aurigaaristo.taxiportfinal;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.aurigaaristo.taxiportfinal.loginfragment.LoginFragment;
 
@@ -26,5 +26,11 @@ public class LoginActivity extends AppCompatActivity {
         transaction.replace(R.id.frame_login, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        loadFragment(new LoginFragment());
     }
 }
